562159df74a6563c7b9b82b3e62913d9813e666f
Sujit Roy <sujitr@gmail.com>
2018-03-27 13:33:35 -0400

    adding log4j support and tidying up simpleAES engine

--- scrambler/src/main/java/com/sujit/scrambler/engines/SimpleAESEngine.java
+++ scrambler/src/main/java/com/sujit/scrambler/engines/SimpleAESEngine.java
@@ -1,38 +1,28 @@
 package com.sujit.scrambler.engines;
 
-import java.security.SecureRandom;
-import java.security.spec.KeySpec;
-import java.security.AlgorithmParameters;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.InvalidParameterSpecException;
-import java.security.InvalidAlgorithmParameterException;
-import java.util.Base64;
-import java.io.UnsupportedEncodingException;
 import java.io.IOException; 
-import javax.crypto.spec.GCMParameterSpec ;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.SecretKeyFactory; 
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.IvParameterSpec; 
 import javax.crypto.CipherInputStream;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.apache.commons.codec.binary.Hex; 
-import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64OutputStream;
 import org.apache.commons.codec.binary.Base64InputStream; 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SimpleAESEngine implements CryptoEngine {
+	private final static Logger logger = LogManager.getLogger(SimpleAESEngine.class.getName());
+	
     private Cipher dCipher;
     private Cipher eCipher;
     private final int MAX_FILE_BUF;
@@ -47,7 +37,7 @@ public class SimpleAESEngine implements CryptoEngine {
 			SecretKey secretKey = new SecretKeySpec(plainTextKey.getBytes(), "AES");
 			dCipher.init(Cipher.DECRYPT_MODE, secretKey);
 		}catch(InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e){
-		    e.printStackTrace();
+		    logger.error("Error in configuring decryption {} ", e);
 		}
 
     }
@@ -58,7 +48,7 @@ public class SimpleAESEngine implements CryptoEngine {
 			SecretKey secretKey = new SecretKeySpec(plainTextKey.getBytes(), "AES");
 			eCipher.init(Cipher.ENCRYPT_MODE, secretKey);
 		}catch(InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e){
-		    e.printStackTrace();
+		    logger.error("Error in configuring encryption {} ", e);
 		}
     }
     
@@ -68,6 +58,7 @@ public class SimpleAESEngine implements CryptoEngine {
         /* you can also replace the below functionality 
             with a IOUTils copy from a CipherInputStream to Base64OutputStream */
         try (Base64OutputStream encryptedStream = new Base64OutputStream(out);) {
+        	logger.debug("encrypting....");
             while((nread = in.read(inbuf))>0){
                 byte[] trimbuf = new byte [nread];
                 for (int i = 0; i < nread; i++)
@@ -80,8 +71,9 @@ public class SimpleAESEngine implements CryptoEngine {
                 encryptedStream.write(finalBuf); 
                 encryptedStream.flush();
             }
+            logger.debug("encryption of streams complete");
         } catch (IllegalBlockSizeException | BadPaddingException | IOException e) { 
-            e.printStackTrace();    
+            logger.error("error while encrypting streams {}", e);
         }
 
     }
@@ -91,20 +83,21 @@ public class SimpleAESEngine implements CryptoEngine {
 	   int nread = 0;
 	   byte [] inbuf = new byte [MAX_FILE_BUF];
 	   try(CipherInputStream cin = new CipherInputStream(new Base64InputStream(encryptedStream), dCipher);) { 
+		   logger.debug("decrypting....");
 	       while ((nread = cin.read (inbuf)) > 0 )
             {
                 // create a buffer to write with the exact number of bytes read. Otherwise a short read fills inbuf with 0x0
                 byte [] trimbuf = new byte [nread];
                 for (int i = 0; i < nread; i++)
                     trimbuf[i] = inbuf[i];
-    
                 // write out the size-adjusted buffer
                 out.write (trimbuf);
             }
             out.flush();
             // IOUtils.copy(cin,out);  // this can be also used instead
+            logger.debug("decryption of streams complete.");
 	   } catch(IOException e) { 
-	       e.printStackTrace(); 
+	       logger.error("error while decrypting streams {}", e);
 	   }
 	}
 }
