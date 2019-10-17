5fa9278b63961d5174183891e01229fe39877a9e
Sujit Roy <sujitr@gmail.com>
2019-10-17 20:18:13 +0530

    adding more exercise

---
+++ algorithms/src/main/java/com/sujit/excercise/ArrayMergeWithSort.java
@@ -0,0 +1,42 @@
+package com.sujit.excercise;
+
+import java.util.Arrays;
+
+/**
+ * There exists two sorted arrays. First array has enough
+ * capacity to store the second one. We need an algorithm
+ * which would merge second one into first one in a sorted
+ * manner.
+ */
+public class ArrayMergeWithSort {
+    public static void main(String[] args){
+        int[] partA = new int[8];
+        int[] partB = {2,6,9,17,45};
+
+        partA[0] = 1;
+        partA[1] = 4;
+        partA[2] = 7;
+
+        merge(partA, partB, 3, 5);
+        System.out.println(Arrays.toString(partA));
+    }
+
+    public static void merge(int[] partA, int[] partB, int a, int b){
+        int k = (a+b)-1;
+        int p = a-1;
+        int q = b-1;
+        // assumption here is array B has greater number of elements than A
+        while(p>=0 && q>=0){
+            if(partA[p] > partB[q]){
+                partA[k--] = partA[p--];
+            }else{
+                partA[k--] = partB[q--];
+            }
+        }
+        // put the remaining elements of B into A
+        while(q>=0){
+            partA[k--] = partB[q--];
+        }
+    }
+
+}
---
+++ algorithms/src/main/java/com/sujit/excercise/ProductArray.java
@@ -0,0 +1,51 @@
+package com.sujit.excercise;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, return a new array such that each
+ * element at index i of the new array is the product of all the
+ * numbers in the original array except the one at i.
+ *
+ * For example, if our input was [1, 2, 3, 4, 5], the expected
+ * output would be [120, 60, 40, 30, 24]. If our input
+ * was [3, 2, 1], the expected output would be [2, 3, 6].
+ *
+ * Follow-up: what if you can't use division?
+ */
+public class ProductArray {
+    public static void main(String[] args){
+        /* input data */
+        int[] data = {1,2,3,4,5};
+        /* array to hold product of all elements left of any index */
+        int[] lprod = new int[data.length];
+        /* array to hold product of all elements right of any index */
+        int[] rprod = new int[data.length];
+        /* array to hold final product by multiplying both above arrays */
+        int[] result = new int[data.length];
+
+        /*
+        initializing left array's leftmost and right arrays rightmost element
+        as 1. That's because 1 would be the starting number for all products.
+         */
+        lprod[0] = rprod[data.length-1] = 1;
+
+        // populate the left product array
+        for(int i = 1; i < data.length; i++){
+            lprod[i] = lprod[i-1]*data[i-1];
+        }
+        System.out.println("Left product array : "+Arrays.toString(lprod));
+
+        // populate the right product array
+        for(int j = data.length-2; j>=0;j--){
+            rprod[j] = rprod[j+1]*data[j+1];
+        }
+        System.out.println("Right product array : "+Arrays.toString(rprod));
+
+        // multiplying both of them to get desired result array
+        for(int i = 0; i < data.length; i++){
+            result[i] = lprod[i]*rprod[i];
+        }
+        System.out.println("Final product array : "+Arrays.toString(result));
+    }
+}
