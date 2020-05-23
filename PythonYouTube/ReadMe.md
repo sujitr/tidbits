f37b094c6b2741517d166d0b8a6793ec3b88a048
Sujit Roy <sujitr@gmail.com>
2020-05-23 06:10:44 -0400

    adding more details for youtube video downloader script

---
+++ PythonYouTube/ReadMe.md
@@ -0,0 +1,6 @@
+# Python YouTube Download Script
+
+This script uses [pytube](https://python-pytube.readthedocs.io) library. Please make sure its installed in your system before using this script.
+
+As of now this script reads YouTube video URL's from a txt file 'videodownloadlist.txt', one URL per line. Destination folder needs
+to be specified in the script for downloaded videos.
--- PythonYouTube/YouTubeDownLoad.py
+++ PythonYouTube/YouTubeDownLoad.py
@@ -1,22 +1,28 @@
 from pytube import YouTube
 
+# name of the file from which to read the video URL's
 filename = 'videodownloadlist.txt'
+# destination folder name for downloaded videos
+dest_folder = 'C:\\YouTubeDownloads'
+
+# read all the video URL's for downloading
 with open(filename) as f:
     lines = [line.strip() for line in f]
-
 print('Total number of videos : ', len(lines))
-tag = 22
+
+# download them one after another
 count = 0;
 for line in lines:
     yt = YouTube(line)
     print('Downloading - ', yt.title)
+    # selecting the best resolution in mp4 format
     stream = yt.streams\
                 .filter(progressive=True, file_extension='mp4')\
                 .order_by('resolution')\
                 .desc()\
                 .first()
     if stream is not None:
-        stream.download('C:\\ScottVideos\\Morning2')
+        stream.download(dest_folder)
         count = count+1
         print('Downloaded...')
     else:
